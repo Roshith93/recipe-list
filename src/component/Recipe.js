29742773e23ea799aeb63e4a0572c86b4c67e762
roshith93 <r.roshith@gmail.com>
2019-07-19 10:07:17 +0530

    Created recipes page
    the layout for the recipe has been created

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react'
 
 class Recipe extends Component {
   render() {
+    const {image_url, publisher, source_url, title, recipe_id} = this.props.recipe;
     return (
       <React.Fragment>
-        <h1>Recipe</h1>
+        <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+          <div className="card">
+            <img src={image_url} 
+              className="img-card-top"
+              style={{height:"14rem"}}
+              alt="recipe"/>
+              <div className="card-body text-capitalize">
+                <h6>{title}</h6>
+                <h6 className="text-warning text-slanted">Publisher {publisher}</h6>
+              </div>
+              <div className="card-footer">
+                <button 
+                    type="button"
+                    className="btn btn-primary text-capitalioze">
+                      Details
+                </button>
+                <a href={source_url}
+                   className="btn btn-success mx-2 text-capitalize"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                     Recipe URL
+                </a>
+              </div>
+          </div>
+        </div>
       </React.Fragment>
     )
   }
